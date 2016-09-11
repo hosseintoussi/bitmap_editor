4b21ddc2b5df6e03d4cff8857e8bfe146208276f
hosseintoussi <hossein@mindvalley.com>
2016-09-11 21:36:18 +0800

    mock validator on bitmap initialize

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 require './app/bitmap'
+require './app/validators/size'
 
 describe Bitmap do
   describe '.new' do
     it 'sets the table' do
-      bitmap = described_class.new(default_value: 'T')
+      validator = double(Validators::Size)
+      allow(validator).to receive(:validate).and_return(nil)
+
+      bitmap = described_class.new(default_value: 'T', validator: validator)
 
       expect(bitmap.table.row(0).size).to eq(1)
       expect(bitmap.table.column(0).size).to eq(1)

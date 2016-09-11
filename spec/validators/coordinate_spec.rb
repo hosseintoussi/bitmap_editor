b6a9c1e3cfab4a71a4903dff2940a2e21d4fdd72
hosseintoussi <hossein@mindvalley.com>
2016-09-11 21:22:05 +0800

    Added missing method name with describe to specs

--- spec/validators/coordinate_spec.rb
+++ spec/validators/coordinate_spec.rb
@@ -4,24 +4,26 @@ require './app/bitmap'
 require './app/errors'
 
 describe Validators::Coordinate do
-  it 'raises if coordinates are not within image size' do
-    bitmap = double(Bitmap)
-    allow(bitmap).to receive(:rows).and_return(10)
-    allow(bitmap).to receive(:columns).and_return(10)
+  describe '.validate' do
+    it 'raises if coordinates are not within image size' do
+      bitmap = double(Bitmap)
+      allow(bitmap).to receive(:rows).and_return(10)
+      allow(bitmap).to receive(:columns).and_return(10)
 
-    validator = described_class.new(bitmap: bitmap)
+      validator = described_class.new(bitmap: bitmap)
 
-    expect{ validator.validate(x: 11) }.to raise_error(NotValidRow)
-    expect{ validator.validate(y: 11) }.to raise_error(NotValidColumn)
-  end
+      expect{ validator.validate(x: 11) }.to raise_error(NotValidRow)
+      expect{ validator.validate(y: 11) }.to raise_error(NotValidColumn)
+    end
 
-  it 'raises nothing if coordinates are within image size' do
-    bitmap = double(Bitmap)
-    allow(bitmap).to receive(:columns).and_return(10)
-    allow(bitmap).to receive(:rows).and_return(10)
+    it 'raises nothing if coordinates are within image size' do
+      bitmap = double(Bitmap)
+      allow(bitmap).to receive(:columns).and_return(10)
+      allow(bitmap).to receive(:rows).and_return(10)
 
-    validator = described_class.new(bitmap: bitmap)
+      validator = described_class.new(bitmap: bitmap)
 
-    expect(validator.validate(x: 9, y: 9)).to be_nil
+      expect(validator.validate(x: 9, y: 9)).to be_nil
+    end
   end
 end
--- spec/validators/size_spec.rb
+++ spec/validators/size_spec.rb
@@ -3,16 +3,18 @@ require './app/validators/size'
 require './app/errors'
 
 describe Validators::Size do
-  it 'raises if size is not within the allowed range of config' do
-    validator = described_class.new(min: 1, max: 10)
+  describe '.validate' do
+    it 'raises if size is not within the allowed range of config' do
+      validator = described_class.new(min: 1, max: 10)
 
-    expect { validator.validate(x: 11) }.to raise_error(OutOfRangeValue)
-    expect { validator.validate(y: 11) }.to raise_error(OutOfRangeValue)
-  end
+      expect { validator.validate(x: 11) }.to raise_error(OutOfRangeValue)
+      expect { validator.validate(y: 11) }.to raise_error(OutOfRangeValue)
+    end
 
-  it 'raises nothing if size is within the allowed range of config' do
-    validator = described_class.new(min: 1, max: 10)
+    it 'raises nothing if size is within the allowed range of config' do
+      validator = described_class.new(min: 1, max: 10)
 
-    expect(validator.validate(x: 9, y: 4)).to be_nil
+      expect(validator.validate(x: 9, y: 4)).to be_nil
+    end
   end
 end

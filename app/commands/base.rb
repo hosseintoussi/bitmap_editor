267d9f570b7d0888af652d198024e48f293e0570
hosseintoussi <hossein@mindvalley.com>
2016-09-13 23:55:53 +0800

    refactored base class to be able to remove color base class and have all classes inherit from base in commands

--- app/commands/base.rb
+++ app/commands/base.rb
@@ -2,8 +2,18 @@ require './app/bitmap'
 
 module Commands
   class Base
-    def initialize(bitmap:)
+    def initialize(bitmap: Bitmap.new)
       @bitmap = bitmap
+    end
+
+    def call(*args)
+      raise 'This method is not implemented yet!'
+    end
+
+    private
+
+    def sanitize(input)
+      input.to_i - 1
     end
   end
 end
